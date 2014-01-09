5be2998d7944ddc1cc978026744de4bbc7737897
David Pfau <david.pfau@gmail.com>
2014-01-09 15:52:01 -0500

    Started work on replacing constant-variance noise model with linear-variance noise model.

--- demo/demo_morph.m
+++ demo/demo_morph.m
@@ -1,21 +1,23 @@
-sz = [400,150,100];
-if ~exist('data','var')
-    load ../../Ahrens/test_dat_s30_t1-100_smallcrop_xt.txt
-    data = padarray(reshape(test_dat_s30_t1_100_smallcrop_xt,sz),[20,20]);
-    clear test_dat_s30_t1_100_smallcrop_xt
-    sz = size(data);
-end
-
+% sz = [400,150,100];
 % if ~exist('data','var')
-%     load ../../Ahrens/test_dat_s25-34_t1-100_smallercrop_xt.txt
-%     data = reshape(test_dat_s25_34_t1_100_smallercrop_xt,[150,250,10,100]);
-%     clear test_dat_s25_34_t1_100_smallercrop_xt;
-%     data = data(:,:,5,:);
-%     data = padarray(data,[20,20]);
+%     load ../../Ahrens/test_dat_s30_t1-100_smallcrop_xt.txt
+%     data = padarray(reshape(test_dat_s30_t1_100_smallcrop_xt,sz),[20,20]);
+%     clear test_dat_s30_t1_100_smallcrop_xt
 %     sz = size(data);
 % end
 
+if ~exist('data','var')
+    load ../../Ahrens/test_dat_s25-34_t1-100_smallercrop_xt.txt
+    data = reshape(test_dat_s25_34_t1_100_smallercrop_xt,[150,250,10,100]);
+    clear test_dat_s25_34_t1_100_smallercrop_xt;
+    data = data(:,:,5,:);
+    data = padarray(data,[20,20]);
+    sz = size(data);
+end
+
 params.prec = 200; % inverse of noise variance
+params.var_slope = 1;
+params.var_offset = 1; % the relationship between mean and variance in any pixel is approximately variance = var_slope*mean + var_offset
 params.blobsize = 6;
 params.threshold = 1.2;
 params.regmax_dist = 5;
--- morph/update_roi_morph.m
+++ morph/update_roi_morph.m
@@ -5,51 +5,52 @@ function ROI = update_roi_morph(frame,ROI,params,t)
                     params.threshold);
           
 [x,y] = ind2sub(size(regmax),find(regmax));
-k = length(x); % number of regional maxima
-wtrshed_id = zeros(k,1);
-for i = 1:k % match watershed labels with regional maxima
+nMax = length(x); % number of regional maxima
+wtrshed_id = zeros(nMax,1);
+for i = 1:nMax % match watershed labels with regional maxima
     wtrshed_id(i) = wtrshed(x(i),y(i));
 end
-q = length(ROI); % number of ROI from previous steps
-fprintf('...Found %d regional maxima',k);
+nROI = length(ROI); % number of ROI from previous steps
+fprintf('...Found %d regional maxima',nMax);
 
-overlap = zeros(k,q);
-distance = zeros(k,q);
-for i = 1:k % Find which existing ROI fall within a watershed
+overlap = false(nMax,nROI);
+distance = zeros(nMax,nROI);
+for i = 1:nMax % Find which existing ROI fall within a watershed
     region = wtrshed==wtrshed_id(i);
-    for j = 1:q
+    for j = 1:nROI
         distance(i,j) = norm(ROI(j).pos-[x(i),y(i)]);
         overlap(i,j) = any(region(:)&ROI(j).shape(:));
     end
 end
-new_roi = zeros(k,1);
-assignment = q+(1:k);
-intensity = regmax(logical(regmax));
-for i = 1:k
+new_roi = zeros(nMax,1);
+assignment = nROI+(1:nMax);
+intensity = regmax(logical(regmax)); % Height of the regional maxima.
+if nROI > 0
+    ROI_mat = cell2mat(arrayfun(@(x)x.shape(:),ROI,'UniformOutput',0));
+    rates = pinv(ROI_mat)*frame(:);
+    resid_var = ones(numel(frame),1)/params.prec;
+    for j = 1:nROI
+        resid_var(logical(ROI(j).prec)) = resid_var(logical(ROI(j).prec)) + rates(j)^2./ROI(j).prec(logical(ROI(j).prec));
+    end
+    resid = (frame(:) - ROI_mat*rates)./sqrt(resid_var);
+else
+    resid = frame;
+end
+for i = 1:nMax
     j = 0; % Index of the ROI to be merged into, or 0 if there is nothing to merge into
     if any(distance(i,:) < params.regmax_dist)
         [~,j] = min(distance(i,:));
-%         idx = find(overlap(i,:,1)>0.5 | overlap(i,:,2)>0.5);
-%         pval = zeros(length(idx),1);
-%         for j = 1:length(idx)
-%             region = ROI(idx(j)).shape & wtrshed==wtrshed_id(i);
-%             resid = (frame(region)/intensity(i) - ROI(idx(j)).shape(region))./sqrt(1./(params.prec*intensity(i)^2) + 1./ROI(idx(j)).prec(region)); % residual scaled so each pixel should be i.i.d. unit normal
-%             pval(j) = 1 - chi2cdf(norm(resid)^2,nnz(region));
-%         end
-%         j = find(pval>params.pval);
-%         if length(j) > 1 || isempty(j) % If you can't reject anything, it's probably so ambiguous that it's better to toss it out
-%             j = 0;
-%         else
-%             j = idx(j);
-%         end
         assignment(i) = j;
     elseif any(overlap(i,:))
-        idx = find(overlap(i,:));
-        region = wtrshed==wtrshed_id(i);
-        ROI_mat = cell2mat(arrayfun(@(x)ROI(x).shape(region),idx,'UniformOutput',0));
-        rates = pinv(ROI_mat)*frame(region); % approximate firing rate of each ROI inside that patch
-        resid = (frame(region)-ROI_mat*rates)./sqrt(1/params.prec + sum(cell2mat(arrayfun(@(x)rates(x)./ROI(x).prec(pix),idx,'UniformOutput',0)),2));
-        if 1 - chi2cdf(norm(resid)^2,nnz(region)) > params.pval
+        region = wtrshed(:)==wtrshed_id(i)&(sum(ROI_mat(:,overlap(i,:)),2)); % The region inside the watershed that is already included in other ROIs should not be significantly different from background noise, unless there is a new ROI in this watershed
+        if 1 - chi2cdf(norm(resid(region))^2,nnz(region)) > params.pval % If it is not significantly different, we have to pick an ROI to merge it with. Let's go with the one that has the greatest power inside the region of overlap.
+            idx = find(overlap(i,:));
+            pow = zeros(length(idx),1);
+            for ii = 1:length(idx)
+                pow(ii) = norm(rates(idx(ii))*ROI_mat(region,idx(ii)));
+            end
+            [~,jj] = max(pow);
+            j = idx(jj);
         end
     end
     if j ~= 0
@@ -71,38 +72,10 @@ for i = 1:k
     end
 end
 
-% connected = sparse(length(ROI),length(ROI));
-% for i = 1:length(ROI)
-%     for j = i+1:length(ROI)
-%         connected(i,j) = norm(ROI(i).pos-ROI(j).pos)<3 ;%|| ...
-%             %(ROI(i).shape(floor(ROI(j).pos(1)),floor(ROI(j).pos(2))) ~= 0 && ...
-%             % ROI(j).shape(floor(ROI(i).pos(1)),floor(ROI(i).pos(2))) ~= 0);
-%     end
-% end
-% connected = connected + connected';
-% connected(1:length(ROI)+1:end) = 1;
-% [S,C] = graphconncomp(connected);
-% for i = 1:S
-%     ROI_(i).intensity = zeros(params.T,1);
-%     ROI_(i).shape = zeros(size(frame));
-%     ROI_(i).pos = [0,0];
-%     for j = 1:length(ROI)
-%         if C(j)==i
-%             ROI_(i).intensity = ROI_(i).intensity + ROI(j).intensity;
-%             ROI_(i).shape = ROI_(i).shape + sum(ROI(j).intensity) * ROI(j).shape;
-%             ROI_(i).pos = ROI_(i).pos + sum(ROI(j).intensity) * ROI(j).pos;
-%         end
-%     end
-%     ROI_(i).shape = ROI_(i).shape / sum(ROI_(i).intensity);
-%     ROI_(i).pos = ROI_(i).pos / sum(ROI_(i).intensity);
-% end
-% 
-% fprintf('...Added %d new regions of interest, merged %d regions of interest\n',nnz(new_roi),length(ROI)-length(ROI_));
-% ROI = ROI_;
 fprintf('...Added %d new regions of interest\n',nnz(new_roi));
 
 if isfield(params,'optose') && params.optose == true
-    h = zeros(k,1); % handles to the text objects in each frame
+    h = zeros(nMax,1); % handles to the text objects in each frame
     figure(61)
     colormap gray
     hold off
@@ -112,15 +85,8 @@ if isfield(params,'optose') && params.optose == true
     himg = imagesc(label2rgb(wtrshed));
     set(himg,'AlphaData',.25);
     scatter(y,x,50,'ro','LineWidth',2)
-%     for i = 1:k
-%         if assignment(i) ~= 0
-%             h(i) = text(y(i)+10,x(i),num2str(assignment(i)),'Color','k','FontSize',14,'LineWidth',1.5);
-%         end
-%     end
     
     for i = 1:length(ROI)
-        % roi_edge = bwboundaries(ROI(i).shape~=0);
-        % line([roi_edge{1}(:,2),[roi_edge{1}(2:end,2);roi_edge{1}(1,2)]],[roi_edge{1}(:,1),[roi_edge{1}(2:end,1);roi_edge{1}(1,1)]],'Color','r');
         scatter(ROI(i).pos(2),ROI(i).pos(1),100,'bo','LineWidth',2);
     end
     drawnow
