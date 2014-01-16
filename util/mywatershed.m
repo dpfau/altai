3bfa2e651e1ff32eef219236fe0c8a9eacc9d05a
David Pfau <pfau@gdw.local>
2014-01-16 18:01:42 -0500

    Rewrote watershed to operate on sets of indices instead of logical array.

--- util/mywatershed.m
+++ util/mywatershed.m
@@ -1,65 +1,59 @@
 function watershed = mywatershed(I,marker)
 
-watershed = zeros(size(I),'uint16');
-idx = find(marker);
-for id = 1:length(idx)
-    watershed(idx(id)) = id;
+idx = find(marker); % index of the pixels in all watersheds
+label = 1:length(idx); % watershed label for the particular pixel
+n = ndims(I);
+sz = size(I);
+switch n
+    case 2
+        [x,y] = ind2sub(sz,idx);
+    case 3
+        [x,y,z] = ind2sub(sz,idx);
+    otherwise
+        error('Hold your horses, I only implemented this for 2D and 3D')
 end
 
 npix = 1; % number of pixels added to watershed in the last iteration
 while npix > 0
     % compute boundary of watershed
-    boundary = zeros(size(watershed),'uint16');
-    overlap = false(size(watershed));
-    overlap_different = false(size(watershed));
+    boundary       = [];
+    boundary_label = [];
+    overlap_different = [];
     for i = -1:1
         for j = -1:1
-            switch ndims(I)
+            switch n
                 case 2
                     if any([i,j])
-                        delta = ( watershed(i+(2:end-1),j+(2:end-1)) .* uint16( watershed(2:end-1,2:end-1) == 0 ) );
-                        overlap(2:end-1,2:end-1) = overlap(2:end-1,2:end-1) | ...
-                            ( delta & boundary(2:end-1,2:end-1) );
-                        overlap_different(2:end-1,2:end-1) = overlap_different(2:end-1,2:end-1) | ...
-                            ( delta & boundary(2:end-1,2:end-1) & (boundary(2:end-1,2:end-1) ~= delta) );
-                        boundary(2:end-1,2:end-1) = boundary(2:end-1,2:end-1) + ( delta .* uint16( ~overlap(2:end-1,2:end-1) ) );
+                        [idx_shift, ia] = setdiff(sub2ind(sz,min(sz(1),max(1,x+i)),min(sz(2),max(1,y+j))),idx);
+                        [boundary, boundary_label, overlap_different] = grow_boundary(boundary, boundary_label, overlap_different, idx_shift, ia, I, label);
                     end
                 case 3
                     for k = -1:1
                         if any([i,j,k])
-                            delta = ( watershed(i+(2:end-1),j+(2:end-1),k+(2:end-1)) .* uint16( watershed(2:end-1,2:end-1,2:end-1) == 0 ) );
-                            overlap(2:end-1,2:end-1,2:end-1) = overlap(2:end-1,2:end-1,2:end-1) | ...
-                                ( delta & boundary(2:end-1,2:end-1,2:end-1) );
-                            overlap_different(2:end-1,2:end-1,2:end-1) = overlap_different(2:end-1,2:end-1,2:end-1) | ...
-                                ( delta & boundary(2:end-1,2:end-1,2:end-1) & (boundary(2:end-1,2:end-1,2:end-1) ~= delta) );
-                            boundary(2:end-1,2:end-1,2:end-1) = boundary(2:end-1,2:end-1,2:end-1) + ( delta .* uint16( ~overlap(2:end-1,2:end-1,2:end-1) ) );
+                            [idx_shift, ia] = setdiff(sub2ind(sz,min(sz(1),max(1,x+i)),min(sz(2),max(1,y+j)),min(sz(3),max(1,z+k))),idx);
+                            [boundary, boundary_label, overlap_different] = grow_boundary(boundary, boundary_label, overlap_different, idx_shift, ia, I, label);
                         end
                     end
-                otherwise
-                    error('Whoa there, I only implemented this for 2D and 3D');
             end
         end
     end
-    boundary = boundary .* uint16( (I>0) & ~overlap_different );
-    boundary_idx = find(boundary);
-    switch ndims(I)
+    boundary = setdiff(boundary, overlap_different);
+    switch n
         case 2
-            [x,y] = ind2sub(size(I),boundary_idx);
+            [xb,yb]    = ind2sub(sz,boundary);
         case 3
-            [x,y,z] = ind2sub(size(I),boundary_idx);
-        otherwise
-            error('Whoa there, I only implemented this for 2D and 3D');
+            [xb,yb,zb] = ind2sub(sz,boundary);
     end
     
     % find the index and value of the maximum neighbor of each boundary pixel
-    neighbor_val = zeros(size(boundary_idx));
-    neighbor_idx = zeros(size(boundary_idx));
+    neighbor_val = zeros(size(boundary));
+    neighbor_idx = zeros(size(boundary));
     for i = -1:1
         for j = -1:1
             switch ndims(I)
                 case 2
                     if any([i,j])
-                        subs = sub2ind(size(I),x+i,y+j);
+                        subs = sub2ind(sz,min(sz(1),max(1,xb+i)),min(sz(2),max(1,yb+j)));
                         greater = I(subs) > neighbor_val;
                         neighbor_val(greater) = I(subs(greater));
                         neighbor_idx(greater) = subs(greater);
@@ -67,7 +61,7 @@ while npix > 0
                 case 3
                     for k = -1:1
                         if any([i,j,k])
-                            subs = sub2ind(size(I),x+i,y+j,z+k);
+                            subs = sub2ind(sz,min(sz(1),max(1,xb+i)),min(sz(2),max(1,yb+j)),min(sz(3),max(1,zb+k)));
                             greater = I(subs) > neighbor_val;
                             neighbor_val(greater) = I(subs(greater));
                             neighbor_idx(greater) = subs(greater);
@@ -80,8 +74,26 @@ while npix > 0
     end
     
     % if the maximum value is inside this watershed already, add the pixel to the watershed
-    in_watershed = watershed(neighbor_idx);
-    watershed(boundary_idx) = in_watershed;
+    [in_watershed] = ismember(neighbor_idx,idx);
+    idx   = [idx,   boundary(in_watershed)];
+    label = [label, boundary_label(in_watershed)];
     npix = nnz(in_watershed);
     fprintf('.')
 end
+
+
+function [boundary, boundary_label, overlap_different] = grow_boundary(boundary, boundary_label, overlap_different, idx_shift, ia, I, label)
+
+label_shift = label(ia);
+
+nonzero = I(idx_shift)>0;
+idx_shift   = idx_shift(nonzero);
+label_shift = label_shift(nonzero);
+
+[overlap, loc] = ismember(idx_shift,boundary);
+overlap_different_idx = overlap;
+overlap_different_idx(overlap) = boundary_label(loc(overlap)) ~= label_shift(overlap);
+overlap_different = [overlap_different, idx_shift(overlap_different_idx)];
+
+boundary       = [boundary,       idx_shift(~overlap)];
+boundary_label = [boundary_label, label_shift(~overlap)];
