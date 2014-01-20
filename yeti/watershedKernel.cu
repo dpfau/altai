91ff5e4786fe8226a942fe520dc24e7c872d0119
David Pfau <pfau@gdw.local>
2014-01-19 23:34:44 -0500

    Added 3D case to watershed kernel, and abstracted some index manipulation

--- yeti/watershedKernel.cu
+++ yeti/watershedKernel.cu
@@ -19,6 +19,29 @@ __device__ int * myrealloc(int * old, int oldsize, int newsize)
     return newT;
 }
 
+__device__ int checkIndex(int idx, int * offset, int * dims, int ndims) {
+    switch (ndims) {
+        case 2:
+            return (idx % dims[0]) + offset[0] >= 0 && (idx % dims[0]) + offset[0] < dims[0]
+                && (idx / dims[0]) + offset[1] >= 0 && (idx / dims[0]) + offset[1] < dims[1];
+        case 3:
+            return (idx % dims[0])             + offset[0] >= 0 && (idx % dims[0])             + offset[0] < dims[0]
+                && ((idx / dims[0]) % dims[1]) + offset[1] >= 0 && ((idx / dims[0]) % dims[1]) + offset[1] < dims[1] 
+                && (idx / (dims[0] * dims[1])) + offset[2] >= 0 && (idx / (dims[0] * dims[1])) + offset[2] < dims[2];
+    }
+}
+
+__device__ int offsetIndex(int idx, int * offset, int * dims, int ndims) {
+    switch (ndims) {
+        case 2:
+            return (idx % dims[0]) + offset[0] + dims[0] * ((idx / dims[0]) + offset[1]);
+        case 3:
+            return                  (idx % dims[0])              + offset[0]  + 
+                dims[0] *           (((idx / dims[0]) % dims[1]) + offset[1]) + 
+                dims[0] * dims[1] * ((idx / (dims[0] * dims[1])) + offset[2]);
+    }
+}
+
 __global__ void watershedKernel(const float * A, int * B, const int * seedIdx, const int N, const int ndims, const int * dims)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -27,20 +50,21 @@ __global__ void watershedKernel(const float * A, int * B, const int * seedIdx, c
         int npix = 1; // number of pixels in the watershed
         int dpix = 1; // change in number of pixels in this loop
         idx[0] = seedIdx[i];
+        int offset[ndims];
         while (dpix > 0) {
             dpix = 0;
-            for (int k=0; k<npix; k++) {
+            for (int q=0; q<npix; q++) {
                for (int ii=-1; ii<=1; ii++) {
                   for (int jj=-1; jj<=1; jj++) {
                       switch(ndims) {
                           case 2:
-                              if ( (idx[k] % dims[0]) + ii >= 0 && (idx[k] % dims[0]) + ii < dims[0]
-                                && (idx[k] / dims[0]) + jj >= 0 && (idx[k] / dims[0]) + jj < dims[1] ) { // check that index is within bounds
-                                  int new_idx = (idx[k] % dims[0]) + ii + dims[0] * ((idx[k] / dims[0]) + jj);
-                                  for (int kk=0; kk<npix; kk++) { 
+                              offset[0] = ii; offset[1] = jj;
+                              if ( checkIndex(idx[q], offset, dims, ndims) ) { // check that index is within bounds
+                                  int new_idx = offsetIndex(idx[q], offset, dims, ndims);
+                                  for (int qq=0; qq<npix; qq++) { 
                                   // Check that index is not in the list. 
                                   // Linear search is stupid and slow, but I'm not sure how to put a hash table inside CUDA device code
-                                      if (idx[kk]==new_idx) {
+                                      if (idx[qq]==new_idx) {
                                           new_idx = -1;
                                           break;
                                       }
@@ -50,17 +74,17 @@ __global__ void watershedKernel(const float * A, int * B, const int * seedIdx, c
                                           int neighbor_idx = new_idx;
                                           for (int iii=-1; iii<=1; iii++) {
                                               for (int jjj=-1; jjj<=1; jjj++) {
-                                                 if ( (new_idx % dims[0]) + iii >= 0 && (new_idx % dims[0]) + iii < dims[0]
-                                                   && (new_idx / dims[0]) + jjj >= 0 && (new_idx / dims[0]) + jjj < dims[1] ) {
-                                                    int this_idx = (new_idx % dims[0]) + iii + dims[0] * ((new_idx / dims[0]) + jjj);
+                                                 if ( checkIndex(new_idx, offset, dims, ndims) ) {
+                                                    offset[0] = iii; offset[1] = jjj;
+                                                    int this_idx = offsetIndex(new_idx, offset, dims, ndims);
                                                     if (A[this_idx] > A[neighbor_idx]) {
                                                         neighbor_idx = this_idx;
                                                     }
                                                  } 
                                               }
                                           }
-                                          for (int kk=0; kk < npix; kk++) {
-                                              if (idx[kk]==neighbor_idx) {
+                                          for (int qq=0; qq < npix; qq++) {
+                                              if (idx[qq]==neighbor_idx) {
                                                   if ( (npix + dpix) % MEM_BLOCK == 0 ) {
                                                       idx = myrealloc(idx, npix + dpix, npix + dpix + MEM_BLOCK);
                                                   }
@@ -72,6 +96,47 @@ __global__ void watershedKernel(const float * A, int * B, const int * seedIdx, c
                                   }
                               }
                           case 3:
+                              for (int kk=-1; kk<=1; kk++) {
+                                  offset[0] = ii; offset[1] = jj; offset[2] = kk;
+                                  if ( checkIndex(idx[q], offset, dims, ndims) ) { // check that index is within bounds
+                                      int new_idx = offsetIndex(idx[q], offset, dims, ndims);
+                                      for (int qq=0; qq<npix; qq++) { 
+                                      // Check that index is not in the list. 
+                                      // Linear search is stupid and slow, but I'm not sure how to put a hash table inside CUDA device code
+                                          if (idx[qq]==new_idx) {
+                                              new_idx = -1;
+                                              break;
+                                          }
+                                      }
+                                      if (new_idx != -1) {
+                                          if (A[new_idx] > 0.0f) {
+                                              int neighbor_idx = new_idx;
+                                              for (int iii=-1; iii<=1; iii++) {
+                                                  for (int jjj=-1; jjj<=1; jjj++) {
+                                                      for (int kkk=-1; kkk<=1; kkk++) {
+                                                          if ( checkIndex(new_idx, offset, dims, ndims)) {
+                                                              offset[0] = iii; offset[1] = jjj; offset[2] = kkk;
+                                                              int this_idx = offsetIndex(new_idx, offset, dims, ndims);
+                                                              if (A[this_idx] > A[neighbor_idx]) {
+                                                                  neighbor_idx = this_idx;
+                                                              }
+                                                          } 
+                                                      }
+                                                  }
+                                              }
+                                              for (int qq=0; qq < npix; qq++) {
+                                                  if (idx[qq]==neighbor_idx) {
+                                                      if ( (npix + dpix) % MEM_BLOCK == 0 ) {
+                                                          idx = myrealloc(idx, npix + dpix, npix + dpix + MEM_BLOCK);
+                                                      }
+                                                      idx[npix + dpix++] = new_idx;
+                                                      break;
+                                                  }
+                                              }
+                                          }
+                                      }
+                                  }
+                              }
                       }
                   }
                }
