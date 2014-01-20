025e40908602b29b020924ec9e1ff393a2fd4424
David Pfau <pfau@gdw.local>
2014-01-19 20:16:16 -0500

    Debugging watershed kernel

--- yeti/watershedKernel.cu
+++ yeti/watershedKernel.cu
@@ -4,10 +4,10 @@
  * David Pfau, 2014
  */
 
-void __global__ watershedKernel(const float * A, const float * B, const int * seedIdx, const int N, const int ndims, const int * dims)
+__global__ void watershedKernel(float * A, float * B, const int * seedIdx, const int N, const int ndims, const int * dims)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     if (i < N) {
-        B[i] = 2.0 * A[i];
+        B[i] = 2.0f * A[i];
     }
 }
