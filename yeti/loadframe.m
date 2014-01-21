bda9cf544619deba453abc95464c82016242075e
David Pfau <pfau@gdw.local>
2014-01-21 17:41:01 -0500

    Got rid of old loadframe, added path option to loadframe.

--- yeti/loadframe.m
+++ yeti/loadframe.m
@@ -1,9 +1,12 @@
 
-function data = loadframe(t)
+function data = loadframe(t,dataPath)
 
+if nargin < 2
+    dataPath = '/vega/stats/users/dbp2112/ahrens/data';
+end
 data = zeros(1472,2048,41,'uint16');
 for i = 1:41
-    fid = fopen(sprintf('/vega/stats/users/dbp2112/ahrens/data/dff_aligned_T%d_slice%d',t,i),'r');
+    fid = fopen(sprintf('%s/dff_aligned_T%d_slice%d',dataPath,t,i),'r');
     slice = fread(fid,'uint16');
     fclose(fid);
     data(:,:,i) = reshape(slice,1472,2048);
