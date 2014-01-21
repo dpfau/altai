55dd741b91e486293513bb46b7f7caaf9a41896a
David Pfau <dbp2112@elk.cc.columbia.edu>
2014-01-21 01:14:53 -0500

    I do not understand git well enough to do this in a not-hacky way. Resolving merge difference with a hatchet.

---
+++ yeti/demo.m
@@ -0,0 +1,17 @@
+params.sig = 5;
+params.dz = 12.5;
+params.thresh = 0.012;
+params.sz = [1472,2048,41];
+
+wtr = zeros(params.sz,'int32'); % initialize up front for speed
+for t = 1
+    data = loadframe(t);
+    gpuData = gpuArray(data);
+    gpuDataBlur = blur(gpuData, [params.sig, params.sig, params.sig/params.dz]);
+    fprintf('S');
+    clear gpuData; % save space on the GPU
+    gpuRegmax = int32(find(myregionalmax(gpuDataBlur-params.thresh)));
+    fprintf('R');
+    fastwatershed(gather(gpuDataBlur-params.thresh), wtr, gather(gpuRegmax));
+    fprintf('W');
+end
