0f06a01dac6cab6d3df5949e4d054c3d04fad2b9
David Pfau <pfau@dbp.local>
2014-01-30 17:12:35 -0500

    Fixed data slicing in loadframe

--- yeti/loadframe.m
+++ yeti/loadframe.m
@@ -6,13 +6,13 @@ if nargin < 2 || isempty(dataPath)
 end
 if nargin < 3, verbose = false; end
 
-data = zeros(1472,2048,41,'uint16');
+data = zeros(1024,2048,41,'uint16');
 for i = 1:41
     fid = fopen(sprintf('%s/dff_aligned_T%d_slice%d',dataPath,t,i),'r');
     slice = fread(fid,'uint16');
     fclose(fid);
-    data(:,:,i) = reshape(slice,1472,2048);
-    data = data(225:1248,:,:); % The animal is entirely within these bounds, so we can just throw out about 1/3rd of the data.
+    slice = reshape(slice,1472,2048);
+    data(:,:,i) = slice(225:1248,:,:); % The animal is entirely within these bounds, so we can just throw out about 1/3rd of the data.
     if verbose, fprintf('.'); end
 end
 data = (single(data) - 15040)/2^16; % empirically, this seems to be where zero is
