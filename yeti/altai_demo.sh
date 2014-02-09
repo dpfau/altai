51f371281e0b6a23273f85c12e5e39f28f85f515
David Pfau <david.pfau@gmail.com>
2014-02-09 01:35:05 -0500

    Removed gpu specification from submit file

--- yeti/altai_demo.sh
+++ yeti/altai_demo.sh
@@ -5,7 +5,7 @@
 # Torque directives
 #PBS -N altai_demo
 #PBS -W group_list=yetistats
-#PBS -l nodes=1,walltime=1:00:00:00,mem=8192mb,other=gpu
+#PBS -l nodes=1,walltime=1:00:00:00,mem=8192mb
 #PBS -M dbp2112@columbia.edu
 #PBS -m abe
 #PBS -V
