43ea2691f7df8d634fbdfca9fd09b0009225695b
David Pfau <pfau@dbp.local>
2014-02-09 00:07:45 -0500

    Reaching goofy debugging message level of frustration

--- yeti/altai_demo.sh
+++ yeti/altai_demo.sh
@@ -5,7 +5,7 @@
 # Torque directives
 #PBS -N altai_demo
 #PBS -W group_list=yetistats
-#PBS -l walltime=24:00:00,mem=8gb,other=gpu
+#PBS -l nodes=1,walltime=24:00:00,mem=8gb,other=gpu
 #PBS -M dbp2112@columbia.edu
 #PBS -m abe
 #PBS -V
@@ -14,4 +14,5 @@
 #PBS -o localhost:/vega/stats/users/dbp2112/ahrens/results/
 #PBS -e localhost:/vega/stats/users/dbp2112/ahrens/results/
 
+echo "I'm going in!"
 matlab-R2012b -nosplash -nodisplay -nodesktop -r 'demo' > ../../results/matoutfile
