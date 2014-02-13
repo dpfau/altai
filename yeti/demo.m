4a956191401a2b7d0ed33ec03d4afe4c5db077c7
David Pfau <david.pfau@gmail.com>
2014-02-13 02:43:14 -0500

    Cleaned up the code a bit

--- yeti/demo.m
+++ yeti/demo.m
@@ -133,7 +133,8 @@ for t = [25:1000,1:24]
 
                 % See if residual passes Chi^2 test
                 error1 = tryGather(norm(residual(region))^2); % scale of the residual in the overlap between ROI and watershed
-                error2 = (ROICenter(:,nearestNeighbors(i))-[xRegmax(i),yRegmax(i),zRegmax(i)])'/diag([params.minDist^2, params.minDist^2, (params.minDist/params.dz)^2])*(ROICenter(:,nearestNeighbors(i))-[xRegmax(i),yRegmax(i),zRegmax(i)]); % scaled distance between the nearest neighbor ROI and the regional maximum
+                distance = (ROICenter(:,nearestNeighbors(i))-[xRegmax(i),yRegmax(i),zRegmax(i)]);
+                error2 = distance'/diag([params.minDist^2, params.minDist^2, (params.minDist/params.dz)^2])*distance; % scaled distance between the nearest neighbor ROI and the regional maximum
                 if (1 - chi2cdf(error1,tryGather(nnz(region)))) * (1 - chi2cdf(error2,3)) > params.pval
                     % Assign regional maximum to ROI with greatest power
                     pow = zeros(length(allNeighbors{i}),1);
