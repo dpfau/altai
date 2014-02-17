4ed1e7076f4f4f60e580db8d091d8a95bad2c0e6
David Pfau <david.pfau@gmail.com>
2014-02-17 01:47:56 -0500

    Moved save files to their own folder

--- yeti/ratesFromAllFrames.m
+++ yeti/ratesFromAllFrames.m
@@ -1,4 +1,4 @@
 load ROI_results.mat
 data = padarray(loadframe(t),[0,0,1]);
 rates = ratesFromFrame(data,ROIShapes,ROIOffset,numROI);
-save(['rates_' num2str(t)],'rates');
+save(['rates/rates_' num2str(t)],'rates');
